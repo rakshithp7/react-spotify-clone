7ae45947b6ed5c93996559d1b18767a0e495a9ce
Rakshith Poojary <rakshithp7@gmail.com>
2020-08-20 13:50:00 +0530

    implement spotify authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []); // eslint-disable-line
+
+  return <div className="app">{token ? <h1>Logged in!</h1> : <Login />}</div>;
 }
 
 export default App;
