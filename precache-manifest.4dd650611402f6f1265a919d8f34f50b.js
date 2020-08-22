6f38cb71c1ce02384ac1c67b55100e6233f92317
Rakshith Poojary <rakshithp7@gmail.com>
2020-08-22 20:03:19 +0530

    Updates

---
+++ precache-manifest.4dd650611402f6f1265a919d8f34f50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0f73e1e6b3356186f18a5dff583bcb",
+    "url": "/react-spotify-clone/index.html"
+  },
+  {
+    "revision": "386c8c7a51c6d851f81d",
+    "url": "/react-spotify-clone/static/css/main.c631b1e4.chunk.css"
+  },
+  {
+    "revision": "b98b95a6ddba10abfad8",
+    "url": "/react-spotify-clone/static/js/2.17e22710.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-spotify-clone/static/js/2.17e22710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386c8c7a51c6d851f81d",
+    "url": "/react-spotify-clone/static/js/main.fc77acca.chunk.js"
+  },
+  {
+    "revision": "eb4be66a04d9bc7a997e",
+    "url": "/react-spotify-clone/static/js/runtime-main.aff0cd99.js"
+  }
+]);
