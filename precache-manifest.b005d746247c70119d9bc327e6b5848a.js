ca5a00443da8fe790aaf9e2efc35fcd99421b67a
Rakshith Poojary <rakshithp7@gmail.com>
2024-01-21 18:24:31 -0500

    Updates

---
+++ precache-manifest.b005d746247c70119d9bc327e6b5848a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e87b5eba173a3be44731257bd3451c",
+    "url": "/react-spotify-clone/index.html"
+  },
+  {
+    "revision": "c0740d93eae195c8779a",
+    "url": "/react-spotify-clone/static/css/main.2b265c9f.chunk.css"
+  },
+  {
+    "revision": "f113778947cadc425ad2",
+    "url": "/react-spotify-clone/static/js/2.29c78b34.chunk.js"
+  },
+  {
+    "revision": "c0740d93eae195c8779a",
+    "url": "/react-spotify-clone/static/js/main.75a9f6ef.chunk.js"
+  },
+  {
+    "revision": "3d48161d87f635502773",
+    "url": "/react-spotify-clone/static/js/runtime~main.888ac673.js"
+  }
+]);
